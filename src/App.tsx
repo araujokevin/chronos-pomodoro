fa41a7fdb9a22e417ec75da2fd366c23a7824cc7
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-09-12 09:10:41 -0300

    feat: finish heading component and ts children typing (reactnode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { TimerIcon } from 'lucide-react';
 import { Heading } from './components/Heading';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-  console.log('Oi');
-
   return (
     <>
-      <Heading> Olá mundo!</Heading>
+      <Heading>
+        Olá mundo!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto earum
         itaque reprehenderit dignissimos omnis odio soluta corrupti ipsa enim
