b6037365bdad15ce1818d12061cd9a72ebdca9e1
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-09-12 10:14:36 -0300

    feat: create html and css structure for each app section

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto earum
-        itaque reprehenderit dignissimos omnis odio soluta corrupti ipsa enim
-        accusantium.
-      </p>
+      <div className="container">
+        <div className="content">
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
