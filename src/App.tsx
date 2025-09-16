4d9524a718bd421549d6cb7e94d7f02999b19f66
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-09-16 16:35:12 -0300

    feat: create logo component with icon and link

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
+import { Container } from './components/Container/Container';
+import { Heading } from './components/Heading/Heading';
+import { Logo } from './components/Logo/Logo';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <section>MENU</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>LOGO</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>FORM</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>FOOTER</section>
-        </div>
-      </div>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
