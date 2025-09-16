c87452b3041ac05218e4d782c608165ac345f466
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-09-16 20:07:43 -0300

    create homepage form

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,22 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
+
+      <Container>
+        <form className="form">
+          <div className="formRow">
+            <label htmlFor="input">Task</label>
+            <input id="input" type="text" />
+          </div>
+
+          <div className="formRow">Lorem ipsum dolor sit amet.</div>
+          <div className="formRow">Ciclos</div>
+          <div className="formRow"> 0 0 0 0 0 0</div>
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
