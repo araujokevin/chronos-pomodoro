ebfcc5249086a75ab3dff142d86d2db97b8a1895
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-10-09 16:00:15 -0300

    style: import and use component-scoped CSS Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
-    <div>
-      <h1>Olá mundo!</h1>
+    <>
+      <Heading />
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam velit
         sapiente maiores pariatur ab omnis voluptas magnam explicabo, laudantium
         possimus?
       </p>
-    </div>
+    </>
   );
 }
