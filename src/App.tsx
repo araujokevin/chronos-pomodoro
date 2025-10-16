1e3df79c1174ed4bc92739bf8f7f6a95ec888256
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-10-16 15:44:48 -0300

    feat: integrate useContext and useState for state management with Context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,10 @@
 import { Home } from './pages/Home';
-import type { TaskStateModel } from './models/TaskStateModel';
-
-import { useState } from 'react';
 import { TaskContextProvider } from './contexts/TaskContext';
 
 import './styles/theme.css';
 import './styles/global.css';
 
-/*
-export type TaskStateModel = {
-  tasks: TaskModel[];
-  secondsRemaining: number;
-  formattedTimeRemaining: string;
-  activeTask: TaskModel | null;
-  currentCycle: number; // 1 to 8
-  config: {
-    workTime: number;
-    shortBreakTime: number;
-    longBreakTime: number;
-  };
-};
-*/
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedTimeRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
-
 export function App() {
-  const [state, setState] = useState(initialState);
-
   // return <Home />;
 
   return (
