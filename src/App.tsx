9d80cef4e56b51084b42f63214b45b26d00240b6
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-10-16 02:36:46 -0300

    refactor: move real state inside Context

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Home } from './pages/Home';
 import type { TaskStateModel } from './models/TaskStateModel';
 
 import { useState } from 'react';
-import { TaskContext } from './contexts/TaskContext';
+import { TaskContextProvider } from './contexts/TaskContext';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -41,8 +41,8 @@ export function App() {
   // return <Home />;
 
   return (
-    <TaskContext.Provider value={{ outraCoisa: 321 }}>
-      <Home />
-    </TaskContext.Provider>
+    <TaskContextProvider>
+      <Home />;
+    </TaskContextProvider>
   );
 }
