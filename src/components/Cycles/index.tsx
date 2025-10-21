d5d4e0220a1eeac06a6a6412aeef8c667ac695fa
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-10-21 18:42:04 -0300

    fix: corrected Cycles, fixed styles import and optimized component

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,38 +1,36 @@
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
-import sytles from './styles.module.css';
+import styles from './styles.module.css';
 
 export function Cycles() {
   const { state } = useTaskContext();
   const cyclesStep = Array.from({ length: state.currentCycle });
 
-  const cyclesDescriptionMap = {
+  const cyclesDescriptionMap: Record<string, string> = {
     workTime: 'foco',
     shortBreakTime: 'descanso curto',
     longBreakTime: 'descanso longo',
   };
 
   return (
-    <div className={sytles.cycles}>
+    <div className={styles.cycles}>
       <span>Ciclos:</span>
 
-      <div className={sytles.cycleDots}>
-        {/* <span className={`${sytles.cycleDot} ${sytles.workTime}`}></span> */}
-
+      <div className={styles.cycleDots}>
         {cyclesStep.map((_, index) => {
           const nextCycle = getNextCycle(index);
           const nextCycleType = getNextCycleType(nextCycle);
 
+          const description = cyclesDescriptionMap[nextCycleType] ?? 'ciclo';
+
           return (
             <span
               key={`${nextCycleType}_${nextCycle}`}
-              className={`${sytles.cycleDot} ${sytles[nextCycleType]}`}
-              aria-label={`Indicador de ciclo de ${cyclesDescriptionMap[nextCycleType]}`}
-              title={`Indicador de ciclo de ${cyclesDescriptionMap[nextCycleType]}`}
-            >
-              {' '}
-            </span>
+              className={`${styles.cycleDot} ${styles[nextCycleType]}`}
+              aria-label={`Indicador de ciclo de ${description}`}
+              title={`Indicador de ciclo de ${description}`}
+            ></span>
           );
         })}
       </div>
