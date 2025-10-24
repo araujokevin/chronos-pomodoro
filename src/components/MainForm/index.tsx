b8fe708f4190d6fdb8c2015616c3701515322f71
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-10-24 20:00:44 -0300

    refactor: migrate state from useState to useReducer

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -7,10 +7,11 @@ import type { TaskModel } from '../../models/TaskModel';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
-import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
+// import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
+import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 
 export function MainForm() {
-  const { state, setState } = useTaskContext();
+  const { state, dispatch } = useTaskContext();
   const taskNameRef = useRef<HTMLInputElement>(null);
 
   // Configurações e cálculo do próximo ciclo
@@ -39,32 +40,11 @@ export function MainForm() {
       type: nextCycleType,
     };
 
-    const secondsRemaining = newTask.duration * 60;
-
-    setState(prevState => ({
-      ...prevState,
-      config: { ...prevState.config },
-      activeTask: newTask,
-      currentCycle: nextCycle,
-      secondsRemaining,
-      formattedTimeRemaining: formatSecondsToMinutes(secondsRemaining),
-      tasks: [...prevState.tasks, newTask],
-    }));
+    dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
   }
 
   function handleInterruptedTask() {
-    setState(prevState => ({
-      ...prevState,
-      activeTask: null,
-      secondsRemaining: 0,
-      formattedTimeRemaining: '00:00',
-      tasks: prevState.tasks.map(task => {
-        if (prevState.activeTask && prevState.activeTask.id === task.id) {
-          return { ...task, interruptDate: Date.now() };
-        }
-        return task;
-      }),
-    }));
+    dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
 
   return (
--- src/contexts/TaskContext/taskReducer.ts
+++ src/contexts/TaskContext/taskReducer.ts
@@ -1,4 +1,6 @@
 import type { TaskStateModel } from '../../models/TaskStateModel';
+import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
+import { getNextCycle } from '../../utils/getNextCycle';
 import { TaskActionTypes, type TaskActionModel } from './taskActions';
 
 export function taskReducer(
@@ -7,10 +9,42 @@ export function taskReducer(
 ): TaskStateModel {
   switch (action.type) {
     case TaskActionTypes.START_TASK: {
-      return state;
+      const newTask = action.payload;
+      const nextCycle = getNextCycle(state.currentCycle);
+      const secondsRemaining = newTask.duration * 60;
+
+      return {
+        ...state,
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedTimeRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...state.tasks, newTask],
+      };
     }
     case TaskActionTypes.INTERRUPT_TASK: {
-      return state;
+      //   ...prevState,
+      //   activeTask: null,
+      //   secondsRemaining: 0,
+      //   formattedTimeRemaining: '00:00',
+      //   tasks: prevState.tasks.map(task => {
+      //     if (prevState.activeTask && prevState.activeTask.id === task.id) {
+      //       return { ...task, interruptDate: Date.now() };
+      //     }
+      //     return task;
+      //   }),
+      return {
+        ...state,
+        activeTask: null,
+        secondsRemaining: 0,
+        formattedTimeRemaining: '00:00',
+        tasks: state.tasks.map(task => {
+          if (state.activeTask && state.activeTask.id === task.id) {
+            return { ...task, interruptDate: Date.now() };
+          }
+          return task;
+        }),
+      };
     }
     case TaskActionTypes.RESET_STATE: {
       return state;
