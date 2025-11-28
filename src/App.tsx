45e5e6c814fa55970f7c62a8db777c091826e3fe
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-11-28 12:25:11 -0300

    feat(ui): import and use CSS Modules with component-scoped styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { Heading } from './components/Heading';
+
 export function App() {
   console.log('oi');
   return (
-    <div>
-      <h1>Olá mundo</h1>
+    <>
+      <Heading />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt et sint
         accusamus quisquam dicta ipsam, iste perferendis quaerat consequatur at.
       </p>
-    </div>
+    </>
   );
 }
