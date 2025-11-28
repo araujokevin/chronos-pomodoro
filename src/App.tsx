b35fb963b732e58b394bc0b31ceb5ac2991b2d04
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-11-28 14:12:20 -0300

    feat: add props and children support to React components

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ export function App() {
   console.log('oi');
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2='String'>
+        Olá, mundo 1
+      </Heading>
+
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt et sint
         accusamus quisquam dicta ipsam, iste perferendis quaerat consequatur at.
