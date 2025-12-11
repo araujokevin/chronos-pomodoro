1d44c09f6ce32ee3d15061d01e5721f0c4237908
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-12-11 11:57:08 -0300

    feat(ui): create footer component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { DefaultButton } from './components/DefaultButton';
 import { DefaultInput } from './components/DefaultInput';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { Footer } from './components/Footer';
 export function App() {
   return (
     <>
@@ -44,6 +45,10 @@ export function App() {
             <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a href='#'>Entenda como funciona a técnica pomodoro</a>
+      <a href='#'>Choronos Pomodoro &copy; {new Date().getFullYear()}</a>
+    </footer>
+  );
+}
