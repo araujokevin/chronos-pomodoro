3adf00bf5df2053f5867c9cb982239d13a899786
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-12-16 08:42:56 -0300

    feat(pages): create Home page and shared template for all pages

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from '../../components/Container';
+import { MainTemplate } from '../../templates/MainTemplate';
+
+export function NotFound() {
+  return (
+    <MainTemplate>
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam
+          maxime neque, inventore quo rerum repellat laboriosam veritatis nam
+          officiis? Amet, dignissimos placeat! Voluptatem incidunt accusantium
+          alias, consectetur aut cum obcaecati?
+        </p>
+      </Container>
+    </MainTemplate>
+  );
+}
