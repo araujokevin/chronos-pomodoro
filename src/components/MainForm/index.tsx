4d938293d48cb5d3d81668abeb36eafac64f426e
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-12-23 03:31:10 -0300

    feat(context): combine useContext and useState for state management

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,30 +2,48 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useTaskContext } from '../../contexts/TaskContext';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+
+  function handleClick() {
+    setState(prevState => {
+      return {
+        ...prevState,
+        formattedSecondsRemaining: '21:00',
+      };
+    });
+  }
+
   return (
-    <form className='form'>
-      <div className='formRow'>
-        <DefaultInput
-          id='meuInput'
-          type='text'
-          labelText='Task'
-          placeholder='Digite algo'
-        />
-      </div>
+    <>
+      <button type='button' onClick={handleClick}>
+        Clicar
+      </button>
 
-      <div className='formRow'>
-        <p>Próximo intervalo é de X min</p>
-      </div>
+      <form className='form'>
+        <div className='formRow'>
+          <DefaultInput
+            id='meuInput'
+            type='text'
+            labelText='Task'
+            placeholder='Digite algo'
+          />
+        </div>
 
-      <div className='formRow'>
-        <Cycles />
-      </div>
+        <div className='formRow'>
+          <p>Próximo intervalo é de X min</p>
+        </div>
 
-      <div className='formRow'>
-        <DefaultButton icon={<PlayCircleIcon />} />
-      </div>
-    </form>
+        <div className='formRow'>
+          <Cycles />
+        </div>
+
+        <div className='formRow'>
+          <DefaultButton icon={<PlayCircleIcon />} />
+        </div>
+      </form>
+    </>
   );
 }
