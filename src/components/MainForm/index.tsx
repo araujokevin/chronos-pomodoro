13e555d4c20b400a5c0f3c2b45d96c5c09926434
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-12-25 07:00:23 -0300

    feat(forms): implement controlled input with value, onChange, and useState

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,11 +2,14 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useState } from 'react';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('');
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('DEU CERTO');
+    console.log('DEU CERTO', taskName);
   }
 
   return (
@@ -18,6 +21,8 @@ export function MainForm() {
             type='text'
             labelText='Task'
             placeholder='Digite algo'
+            value={taskName}
+            onChange={e => setTaskName(e.target.value)}
           />
         </div>
 
