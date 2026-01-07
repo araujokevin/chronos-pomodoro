b3bd23fae083baec89df1d48cf7dd3539d291c0a
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2026-01-07 20:03:50 -0300

    feat(ui): add React Toastify notifications for user feedback

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -9,6 +9,8 @@ import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 import { Tips } from '../Tips';
+import { toast } from 'react-toastify';
+import { showMessage } from '../../adapters/showMessage';
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -20,13 +22,14 @@ export function MainForm() {
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    showMessage.dismiss();
 
     if (taskNameInput.current === null) return;
 
     const taskName = taskNameInput.current.value.trim();
 
     if (!taskName) {
-      alert('Digite o nome da tarefa');
+      toast.warn('Digite o nome da tarefa');
       return;
     }
 
@@ -41,10 +44,13 @@ export function MainForm() {
     };
 
     dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
+
+    showMessage.success('Tarefa iniciada');
   }
 
   function handleInterruptTask() {
     dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
+    showMessage.error('Tarefa interrrompinda!');
   }
 
   return (
